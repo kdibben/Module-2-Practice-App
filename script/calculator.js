eb4f4014836508a4de523f148a2182d59d942897
Kelsie Dibben <kmdibben@gmail.com>
2019-09-21 21:03:23 -0500

    Completed project

--- script/calculator.js
+++ script/calculator.js
@@ -1,14 +1,14 @@
 function calculate(){
-    const total = parseFloat(document.getElementById('total').value)
+    const totalBill = parseFloat(document.getElementById('total').value)
     const rate = parseFloat(document.getElementById('rate').value)
 
-    const tip = total * (rate / 100)
-    const tax = total * (5.5 / 100)
-    const total = total + tip + tax
+    const tip = totalBill * (rate / 100)
+    const tax = totalBill * (5.5 / 100)
+    const grandTotal = totalBill + tip + tax
 
     document.getElementById('tip-label').innerHTML = `$${tip.toFixed(2)}`
     document.getElementById('tax-label').innerHTML = `$${tax.toFixed(2)}`
-    document.getElementById('grand-total') = `$${total.toFixed(2)}`
+    document.getElementById('grand-total').innerHTML = `$${grandTotal.toFixed(2)}`
 }
 
 document.getElementById('calculateTotal').addEventListener('click', calculate)
